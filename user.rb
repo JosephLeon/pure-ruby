f9e998f4095718cc42bb7925420c1738dfeba19d
Joseph Leon <joseph.leon.9@gmail.com>
2016-04-10 19:46:34 -0400

    Add some comments to the user class.

--- user.rb
+++ user.rb
@@ -1,19 +1,20 @@
 class User
-	attr_reader :id, :first_name, :last_name, :cash, :stocks_owned
+	attr_reader :id, :first_name, :last_name, :cash, :stocks_owned, :stock
 	def initialize(args)
 		@id = args[:id]
 		@first_name = args[:first_name]
 		@last_name = args[:last_name]
 		@cash = args[:cash]
 		@stocks_owned = args[:stocks_owned]
+		@stock = args[:stock]
 	end
 
 	def list_stocks_owned
-
+		# return stocks owned by a user
 	end
 
 	def add_stock_to_portfolio
-
+		# get snapshot of stock data, save to db referenced to user id
 	end
 
 end
