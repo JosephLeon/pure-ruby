05ff2f4db3b18414d317929c152303514a87fa32
Joseph Leon <joseph.leon.9@gmail.com>
2016-04-11 08:59:58 -0400

    Add random generated stock information.

--- lib/stockbuddy/stock_quote.rb
+++ lib/stockbuddy/stock_quote.rb
@@ -4,15 +4,27 @@ class StockQuote
 		@ticker = args[:ticker]
 	end
 
-	def get_quote(ticker)
+	def get_quote()
 		# using ticker hit external api and get data
 		# for now we will hardcode return values
+		tickers = [:SBUX, :WFC, :BP, :NFLX, :FSLR]
+		rndm = Random.new
 		data = {
-			"ticker" => "SBUX",
-			"price" => "61.04",
-			"eps" => "1.63",
+			"ticker" => tickers.sample,
+			"price" => (rndm.rand(5.00..120.00)*100).round / 100.0,
+			"eps" => (rndm.rand(-1.00..25.00)*100).round / 100.0,
 			"time" => Time.now.getutc,
 		}
 		return data
 	end
+
+	def self.all
+    ObjectSpace.each_object(self).to_a
+  end
+
+  def self.count
+    all.count
+  end
 end
+
+puts StockQuote.new(:ticker => "TICK").get_quote
