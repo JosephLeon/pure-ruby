241cb299999145512dbcaab6b6b42e93bf1b49e9
Joseph Leon <joseph.leon.9@gmail.com>
2016-04-17 18:01:35 -0400

    Test.

--- lib/stockbuddy/purchase.rb
+++ lib/stockbuddy/purchase.rb
@@ -1,9 +1,9 @@
 class Purchase
-	attr_reader :amount, :price
-	def initialize(args)
-		@amount => args[:amount]
-		@data => args[:price]
-	end
+	# attr_reader :amount, :price
+	# def initialize(args)
+	# 	@amount => args[:amount]
+	# 	@data => args[:price]
+	# end
 
 	def fake_method
 		puts "Fake method"
