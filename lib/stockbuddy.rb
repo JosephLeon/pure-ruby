9c48bbfc9305230e9ee101d96591d52b66c9bbf5
Joseph Leon <joseph.leon.9@gmail.com>
2016-04-17 22:39:43 -0400

    Comment out code not associated with user.

--- lib/stockbuddy.rb
+++ lib/stockbuddy.rb
@@ -6,15 +6,15 @@ require 'stockbuddy/purchase'
 
 # puts User.make_purchase("FAKE", 20)
 
-@this_stock = Stock.new(
-	:ticker => 'MITL', 
-	:price => 8.21,
-	:revenue => 1157700,
-	:cost_of_revenue => 543800,
-	:eps => -0.17)
+# @this_stock = Stock.new(
+# 	:ticker => 'MITL', 
+# 	:price => 8.21,
+# 	:revenue => 1157700,
+# 	:cost_of_revenue => 543800,
+# 	:eps => -0.17)
 
-puts @this_stock.gross_profit
-puts @this_stock.should_i_buy_you
-puts @this_stock.evaluate_eps
+# puts @this_stock.gross_profit
+# puts @this_stock.should_i_buy_you
+# puts @this_stock.evaluate_eps
 
 puts StockQuote.new().get_quote("FOO")
