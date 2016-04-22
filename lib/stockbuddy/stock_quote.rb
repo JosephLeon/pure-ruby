547d526d313fbcc05646885db00724f4bbf61fcc
Joseph Leon <joseph.leon.9@gmail.com>
2016-04-22 09:24:37 -0400

    Refactor StockQuote get_quote method so it references self, move the instance variables outside the get_quote method. Add subclass that does nothing. ;

--- lib/stockbuddy/stock_quote.rb
+++ lib/stockbuddy/stock_quote.rb
@@ -1,14 +1,13 @@
 class StockQuote
 
-	def get_quote(ticker)
-		# using ticker hit external api and get data
-		# for now we will hardcode return values
-		tickers = [:SBUX, :WFC, :BP, :NFLX, :FSLR]
-		rndm = Random.new
+	@tickers = [:SBUX, :WFC, :BP, :NFLX, :FSLR]
+	@rndm = Random.new
+
+	def self.get_quote(ticker)
 		data = {
-			"ticker" => tickers.sample,
-			"price" => (rndm.rand(5.00..120.00)*100).round / 100.0,
-			"eps" => (rndm.rand(-1.00..25.00)*100).round / 100.0,
+			"ticker" => @tickers.sample,
+			"price" => (@rndm.rand(5.00..120.00)*100).round / 100.0,
+			"eps" => (@rndm.rand(-1.00..25.00)*100).round / 100.0,
 			"time" => Time.now.getutc,
 		}
 		return data
@@ -18,5 +17,10 @@ end
 
 
 class DetailedStockQuote < StockQuote
+
+	def self.get_quote(ticker)
+		super
+		# super + data[:peg] = (rndm.rand(-1.00..25.00)*100).round / 100.0
+	end
 
 end
