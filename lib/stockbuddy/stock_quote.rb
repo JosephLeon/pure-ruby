768363084afc07723278f7a5a0a304f5a5f02594
Joseph Leon <joseph.leon.9@gmail.com>
2016-04-23 20:21:06 -0400

    Add private method for authenticating the ticker, check if super returns nil in subclass DetailedStockQuote.

--- lib/stockbuddy/stock_quote.rb
+++ lib/stockbuddy/stock_quote.rb
@@ -1,22 +1,35 @@
 class StockQuote
 	@@rndm = Random.new
 	def StockQuote.get_quote(ticker)
-		tickers = [:SBUX, :WFC, :BP, :NFLX, :FSLR]
-		@data = {
-			"ticker" => tickers.sample,
-			"price" => (@@rndm.rand(5.00..120.00)*100).round / 100.0,
-			"eps" => (@@rndm.rand(-1.00..25.00)*100).round / 100.0,
-			"time" => Time.now.getutc,
-		}
-		return @data
+		if self.authenticate_ticker(ticker)
+			tickers = [:SBUX, :WFC, :BP, :NFLX, :FSLR]
+			@data = {
+				"ticker" => tickers.sample,
+				"price" => (@@rndm.rand(5.00..120.00)*100).round / 100.0,
+				"eps" => (@@rndm.rand(-1.00..25.00)*100).round / 100.0,
+				"time" => Time.now.getutc,
+			}
+			return @data
+		else
+			return nil
+		end
 	end
+
+	private
+		def StockQuote.authenticate_ticker(ticker)
+			ticker
+		end
 end
 
 
 class DetailedStockQuote < StockQuote
 	def DetailedStockQuote.get_quote(ticker)
 		super
-		data = super
+		if super.nil?
+			return
+		else
+			data = super.dup
+		end
 		data["peg"] = (@@rndm.rand(-1.00..25.00)*100).round / 100.0
 		data["dividends"] = (@@rndm.rand(0.00...10.00)*100).round / 100.0
 		puts data.inspect
